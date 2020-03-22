24491e77e7a71e418853f8134116893e32a1c2f7
Guilherme Strutzki <guilherme.strutzki@shareeat.com.br>
2020-03-21 23:43:10 -0300

    :sparkles: Add RN-chart in iOS

---
+++ ios/File.swift
@@ -0,0 +1,9 @@
+//
+//  File.swift
+//  coronavirus_information
+//
+//  Created by Guilherme Strutzki on 21/03/20.
+//  Copyright © 2020 Facebook. All rights reserved.
+//
+
+import Foundation
--- src/features/Preload/index.js
+++ src/features/Preload/index.js
@@ -1,12 +1,55 @@
 import React from 'react';
-import {View, Text} from 'react-native';
+import {StyleSheet, View} from 'react-native';
+import {LineChart} from 'react-native-charts-wrapper';
 
 const Preload = () => {
   return (
-    <View>
-      <Text>asdlsaldasldlas</Text>
+    <View style={{flex: 1}}>
+      <View style={styles.container}>
+        <LineChart
+          style={styles.chart}
+          data={{
+            dataSets: [
+              {
+                label: 'demo',
+                config: {fillColor: '#00ff'},
+                values: [
+                  {x: 5, y: 90},
+                  {x: 10, y: 130},
+                  {x: 50, y: 2000, marker: 'eat more'},
+                  {x: 80, y: 9000, marker: 'eat less'},
+                  ,
+                ],
+              },
+              {
+                label: 'demo 2',
+                values: [
+                  {x: 20, y: 30},
+                  {x: 10, y: 130},
+                  {x: 50, y: 2000, marker: 'eat more'},
+                  {x: 80, y: 9000, marker: 'eat less'},
+                  ,
+                ],
+              },
+            ],
+          }}
+        />
+      </View>
     </View>
   );
 };
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#F5FCFF',
+  },
+  chart: {
+    width: 300,
+    height: 300,
+  },
+});
 
 export default Preload;
